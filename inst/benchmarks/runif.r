e3b87da1cce93c61a533180e887ac6b837afe534
wrathematics <wrathematics@gmail.com>
2017-11-15 08:12:41 -0500

    update benchmarks

--- inst/benchmarks/runif.r
+++ inst/benchmarks/runif.r
@@ -2,17 +2,17 @@ n = 1e8
 
 f = function(label, time, digits=3) cat(paste0(label, ": ", round(time, digits=digits), "\n"))
 
-t.r = system.time(rnorm(n))
+t.r = system.time(runif(n))
 f("R", t.r[[3]])
 
-t.fastrand = system.time(fastrand::rnorm(n))
+t.fastrand = system.time(fastrand::runif(n))
 f("\nfastrand", t.fastrand[[3]])
 
 f("relative", t.r[[3]] / t.fastrand[[3]])
 
 
 
-t.fastrand = system.time(fastrand::rnorm(n, type="float"))
+t.fastrand = system.time(fastrand::runif(n, type="float"))
 f("\nfastrand (float)", t.fastrand[[3]])
 
 f("relative", t.r[[3]] / t.fastrand[[3]])
