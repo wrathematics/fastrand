db2408b2c6cfa4a046631f5ee8c6de7b7fad5090
wrathematics <wrathematics@gmail.com>
2017-12-11 14:39:59 -0500

    add nan setter

--- src/fastrand_native.c
+++ src/fastrand_native.c
@@ -10,6 +10,7 @@ extern SEXP R_fast_rnorm(SEXP n1_, SEXP n2_, SEXP mean_, SEXP sd_, SEXP seed_, S
 extern SEXP R_fast_runif(SEXP n1_, SEXP n2_, SEXP min_, SEXP max_, SEXP seed_, SEXP nthreads);
 extern SEXP R_get_backend();
 extern SEXP R_get_num_threads();
+extern SEXP R_fastrand_setnan(SEXP n1_, SEXP n2_, SEXP type_);
 
 static const R_CallMethodDef CallEntries[] = {
   {"R_getseed", (DL_FUNC) &R_getseed, 3},
@@ -17,6 +18,7 @@ static const R_CallMethodDef CallEntries[] = {
   {"R_fast_rnorm", (DL_FUNC) &R_fast_runif, 6},
   {"R_get_backend", (DL_FUNC) &R_get_backend, 0},
   {"R_get_num_threads", (DL_FUNC) &R_get_num_threads, 0},
+  {"R_fastrand_setnan", (DL_FUNC) &R_fastrand_setnan, 0},
   {NULL, NULL, 0}
 };
 
